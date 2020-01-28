86bbb70883c39e5ba79bed44b15a80fa9ff4f07a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-28 09:52:25 -0700

    Implement `counterMakerWithLimit` function

--- index.js
+++ index.js
@@ -275,8 +275,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(limit) {
+  let count = 0;
+  return function() {
+      if (count > limit) {
+          count = 0;
+      }
+      return count++;
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
